19ba08b6d3fc6e28714706e364d3e8d9b094c4b2
hieutrung2207 <hieutrung22071999@gmail.com>
2023-02-09 09:43:49 +0700

    trung

--- app/src/main/java/com/example/baostore/activities/MainActivity.java
+++ app/src/main/java/com/example/baostore/activities/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //trung 1
+        //trung 2
 
     }
 }
