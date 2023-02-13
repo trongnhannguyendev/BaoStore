9a6b80a3a6ee91916a507acefa01c8d405e0af2a
hieutrung2207 <hieutrung22071999@gmail.com>
2023-02-13 16:49:12 +0700

    update layout

---
+++ app/src/main/java/com/example/baostore/activities/BuyHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.baostore.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.baostore.R;
+
+public class BuyHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_history);
+    }
+}
