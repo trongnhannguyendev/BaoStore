41b4119212f6ead297fccf7debaa673f19336a58
hieutrung2207 <hieutrung22071999@gmail.com>
2023-02-23 12:00:25 +0700

    trung

--- app/src/main/java/com/example/baostore/activities/MainActivity.java
+++ app/src/main/java/com/example/baostore/activities/MainActivity.java
@@ -1,31 +1,103 @@
 package com.example.baostore.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import com.example.baostore.R;
 import com.example.baostore.fragments.CartFragment;
 import com.example.baostore.fragments.HomeFragment;
+import com.example.baostore.fragments.ProfileFragment;
 import com.example.baostore.fragments.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
+    Toolbar myToolbar;
+     ImageView imgBack;
+     TextView tvTitleHeader;
+     Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // header
+        tvTitleHeader=findViewById(R.id.title);
+        tvTitleHeader.setText("Trang chủ");
+
+        // button back
+        imgBack=findViewById(R.id.back_button);
+        imgBack.setVisibility(View.GONE);
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
+        bottomNavigationView = findViewById(R.id.myBottomNav);
+       myToolbar=findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+
+
+
+
+
+
+
+        // Thêm HomeFragment vào FrameLayout
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeFragment()).commit();
+
+        // Title toolbar
+
+
+        //Điều hướng navigation
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.Home:
+
+                    tvTitleHeader.setText("Trang chủ");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeFragment()).commit();
+                    return true;
+                case R.id.Search:
+
+                    tvTitleHeader.setText("Tìm kiếm");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new SearchFragment()).commit();
+                    return true;
+                case R.id.Cart:
+
+                    tvTitleHeader.setText("Giỏ hàng");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new CartFragment()).commit();
+                    return true;
+                case R.id.User:
+
+                    tvTitleHeader.setText("Người dùng");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new ProfileFragment()).commit();
+                    return true;
+            }
+            return false;
+        });
+
+
+
+
+
+
+
+
+
     }
-
-
 }
--- app/src/main/java/com/example/baostore/fragments/HomeFragment.java
+++ app/src/main/java/com/example/baostore/fragments/HomeFragment.java
@@ -6,109 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.baostore.DAOs.TempBookDAO;
 import com.example.baostore.R;
-import com.example.baostore.adapters.Book2Adapter;
-import com.example.baostore.adapters.BookAdapter;
-import com.example.baostore.models.Book;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class HomeFragment extends Fragment {
-    List<Book> list_book;
-    RecyclerView recyBook_Popular, recyBook_New;
-    BookAdapter adapter;
-    Book2Adapter book2Adapter;
-    TempBookDAO tempBookDAO;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.fragment_home, container, false);
-
-        list_book = new ArrayList<>();
-
-        recyBook_Popular = v.findViewById(R.id.recyBook_Popular);
-        recyBook_New = v.findViewById(R.id.recyBook_new);
-
-        tempBookDAO = new TempBookDAO(getContext(),this,recyBook_Popular,recyBook_New);
-
-        recyBook_Popular.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
-        recyBook_New.setLayoutManager(new LinearLayoutManager(getContext()));
-
-
-
-        tempBookDAO.getBooks();
-
-        recyBook_Popular.setAdapter(adapter);
-        recyBook_New.setAdapter(book2Adapter);
-
-        return v;
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
-    public void showBooks(){
-        //list_book = bookDAO.getBooks();
-        adapter = new BookAdapter(list_book,getContext());
-        book2Adapter = new Book2Adapter(list_book,getContext());
-
-        recyBook_Popular.setAdapter(adapter);
-        recyBook_New.setAdapter(book2Adapter);
-
-        adapter.notifyDataSetChanged();
-    }
-
-    /*
-    private void getBooks() {
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(ApiUrl.BASE)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        ApiService service = retrofit.create(ApiService.class);
-
-        Call<Result> call = service.getbook();
-        call.enqueue(new Callback<Result>() {
-            @Override
-            public void onResponse(Call<Result> call, Response<Result> response) {
-                JsonArray myArr = response.body().getData();
-
-                Log.d("------------------------", myArr.size()+"");
-                for(JsonElement jsonElement: myArr){
-                    JsonObject jsonObject = jsonElement.getAsJsonObject();
-                    int bookID = jsonObject.get("bookid").getAsInt();
-                    String title = jsonObject.get("title").getAsString();
-                    double price = jsonObject.get("price").getAsDouble();
-                    String url = jsonObject.get("url").getAsString();
-
-                    Book book = new Book(bookID,title, price,url);
-                    Log.d("--------------------",book.getTitle());
-                    list_book.add(book);
-
-                }
-
-
-                adapter = new BookAdapter(list_book,getContext());
-                book2Adapter = new Book2Adapter(list_book,getContext());
-
-                recyBook_Popular.setAdapter(adapter);
-                recyBook_New.setAdapter(book2Adapter);
-
-                adapter.notifyDataSetChanged();
-                book2Adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(Call<Result> call, Throwable t) {
-                Toast.makeText(getContext(), "An error has occured", Toast.LENGTH_LONG).show();
-            }
-
-
-        });
-        }
-     */
 }
