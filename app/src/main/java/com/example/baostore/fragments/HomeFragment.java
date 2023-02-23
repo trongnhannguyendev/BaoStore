605af47aebd42c82791d5988ab47996b36211fbe
hieutrung2207 <hieutrung22071999@gmail.com>
2023-02-23 12:00:25 +0700

    trung

--- app/src/main/java/com/example/baostore/fragments/HomeFragment.java
+++ app/src/main/java/com/example/baostore/fragments/HomeFragment.java
@@ -1,19 +1,73 @@
 package com.example.baostore.fragments;
 
+
+
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
 import com.example.baostore.R;
+import com.google.firebase.database.core.Tag;
 
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        NestedScrollView nestedScrollView = view.findViewById(R.id.myScrollView);
+        LinearLayout layout1 = view.findViewById(R.id.layout_category_1);
+        LinearLayout layout2 = view.findViewById(R.id.layout_category_2);
+
+
+
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                int[] location = new int[2];
+                layout2.getLocationOnScreen(location);
+//                int[] layoutLocation = new int[2];
+//                layout2.getLocationOnScreen(layoutLocation);
+//
+//                int[] scrollViewLocation = new int[2];
+//                nestedScrollView.getLocationOnScreen(scrollViewLocation);
+//
+//                int distanceFromLayoutTopToScrollViewTop = layoutLocation[1] - scrollViewLocation[1];
+
+
+
+                int y = location[1]-layout2.getHeight()-93;
+                Log.i("TAG",  "Y: "+y);
+                Log.i("TAG", "Show + "+scrollY);
+                if (y<=0){
+                    layout1.setVisibility(View.VISIBLE);
+
+
+                }
+                else {
+                    layout1.setVisibility(View.GONE);
+                    layout2.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+
+
+
+
+        return view;
     }
 }
