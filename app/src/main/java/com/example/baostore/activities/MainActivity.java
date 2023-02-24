080b6856aaea9f53e69d9731ac1ce819dc5d1fca
minhdang210799 <98717844+minhdang210799@users.noreply.github.com>
2023-02-24 21:50:39 +0700

    sth

--- app/src/main/java/com/example/baostore/activities/MainActivity.java
+++ app/src/main/java/com/example/baostore/activities/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.baostore.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.baostore.R;
+import com.example.baostore.fragments.CartFragment;
+import com.example.baostore.fragments.HomeFragment;
+import com.example.baostore.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
+
+
 }
