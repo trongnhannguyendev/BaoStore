d4a667f019194c517a5a1f9df180ae16c3a3a597
trongnhannguyendev <124512127+trongnhannguyendev@users.noreply.github.com>
2023-03-11 21:48:15 +0700

    add 2 packed

---
+++ app/src/main/java/com/example/baostore/Utils/AppConstant.java
@@ -0,0 +1,11 @@
+package com.example.baostore.Utils;
+
+public class AppConstant {
+    public static final String USER_LOGIN_EMAIL = "USER_LOGIN_EMAIL";
+    public static final String IS_USER_LOGIN = "IS_USER_LOGIN";
+    public static final String APP_NAME = "PLIFEAPP";
+    public static final String USER_LOGIN_TOKEN = "USER_LOGIN_TOKEN";
+    public static final String API_SERVER_HOST_ADDRESS = "http://10.0.2.2";
+    public static final String API_SERVER_PORT_NUMBER = "9999";
+
+}
